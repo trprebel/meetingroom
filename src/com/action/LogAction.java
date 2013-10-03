17882601500aecaa8dd4355eb91a83255046aa68
trprebel <610991310@qq.com>
2013-10-03 15:00:01 +0800

    paging

--- src/com/action/LogAction.java
+++ src/com/action/LogAction.java
@@ -7,8 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.struts2.ServletActionContext;
 
 import com.bean.Log;
+import com.page.Program;
 import com.dao.impl.LogDao;
 import com.opensymphony.xwork2.ActionSupport;
+import com.page.Paginator;
 
 public class LogAction extends ActionSupport{
 
@@ -16,20 +18,52 @@ public class LogAction extends ActionSupport{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	public Paginator paginator=new Paginator();
+	private Program program=new Program();
 	public String requestlog()
 	{
 		HttpServletRequest request=ServletActionContext.getRequest();
 		try
 		{
 			LogDao logdao=new LogDao();
-			List<Log> logs=logdao.findAllLog();
-			request.setAttribute("logs", logs);
+			
+			//request.setAttribute("logs", logs);
+			
+			//Paginator paginator=new Paginator();
+			//Program program=new Program();
+			program.setStart(paginator.getOffset());
+			program.setLenth(paginator.getPageSize());
+		
+			int allRow = logdao.findPageCount();
+			//System.out.println(allRow);
+			if(allRow==0){
+				paginator.setData(0, null);
+				return "success";
+			}
+			List<Log> logs=logdao.findLogList(program);
+			//List list = videoDao.findPageList(program, Constants.IBATIS_VIDEO);
+			paginator.setData(allRow, logs);
+			
+			
+			
 			return "success";
 		}
 		catch (Exception e){
 			e.printStackTrace();
 			return "error";
 		}
+	}
+	public Paginator getPaginator() {
+		return paginator;
+	}
+	public void setPaginator(Paginator paginator) {
+		this.paginator = paginator;
+	}
+	public Program getProgram() {
+		return program;
+	}
+	public void setProgram(Program program) {
+		this.program = program;
 	}
 
 }
--- src/com/dao/impl/LogDao.java
+++ src/com/dao/impl/LogDao.java
@@ -3,13 +3,19 @@ package com.dao.impl;
 import java.util.List;
 
 import com.bean.Log;
+import com.page.Program;
 
 public class LogDao {
 
 	public void insertLog(Log log) throws Exception{
 		SqlMap.getSqlMapClient().insert("insertLog",log);
 	}
-	public List<Log> findAllLog() throws Exception{
-		return (List<Log>)SqlMap.getSqlMapClient().queryForList("findAllLog");
+	public List<Log> findLogList(Program program) throws Exception{
+		//System.out.println(program.getStart());
+		//System.out.println(program.getLenth());
+		return (List<Log>)SqlMap.getSqlMapClient().queryForList("findLogList",program);
+	}
+	public Integer findPageCount() throws Exception{
+		return (Integer)SqlMap.getSqlMapClient().queryForObject("findPageCount");
 	}
 }
